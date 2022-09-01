98ab65b61956387a89c1e8def9bd4adf594c335f
Raymond Abid <raymond.abid@hc-sc.gc.ca>
2022-09-01 14:33:29 -0700

    Changing image references

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "null_resource" "docker_push" {
 
 resource "docker_container" "server_container" { 
   name  = "server_container"
-  image = "docker_image.server_image:${var.image_version}"
+  image = "server_image:${var.image_version}"
 
 
   networks_advanced {
@@ -84,7 +84,7 @@ resource "docker_container" "server_container" {
 
 resource "docker_container" "client_container" {
   name  = "client_container"
-  image = "docker_image.client_image:${var.image_version}"
+  image = "client_image:${var.image_version}"
 
 
   networks_advanced {
