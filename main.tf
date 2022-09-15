d380fa7bfd3da7e28e0998ee59f7d7e4a27c5434
Raymond Abid <raymond.abid@hc-sc.gc.ca>
2022-09-15 12:58:16 -0700

    simplifying project

--- main.tf
+++ main.tf
@@ -18,22 +18,14 @@ resource "docker_network" "rays-network" {
 }
 
 # Build docker images for server, client, and proxy
-resource "docker_image" "server_image" {
-  name = "server_image:latest"
+resource "docker_image" "app_image" {
+  name = "app_image:latest"
 
   build {
       path = "."
   }
 }
 
-resource "docker_image" "client_image" {
-  name = "client_image:latest"
-
-  build {
-      path = "./public/"
-  }
-
-}
 
 resource "docker_image" "proxy_image" {
   name = "proxy_image:latest"
@@ -42,27 +34,28 @@ resource "docker_image" "proxy_image" {
       path = "./proxy/"
   }
 }
+
+      # docker tag client_image:latest jack.hc-sc.gc.ca/devops/raytest/client_image:latest
+      # docker push jack.hc-sc.gc.ca/devops/raytest/client_image:latest
       
 # Docker push command to container registry
 resource "null_resource" "docker_push" {
     provisioner "local-exec" {
     command = <<-EOT
-      docker tag server_image:latest jack.hc-sc.gc.ca/devops/raytest/server_image:latest
-      docker push jack.hc-sc.gc.ca/devops/raytest/server_image:latest
-      docker tag client_image:latest jack.hc-sc.gc.ca/devops/raytest/client_image:latest
-      docker push jack.hc-sc.gc.ca/devops/raytest/client_image:latest
+      docker tag app_image:latest jack.hc-sc.gc.ca/devops/raytest/app_image:latest
+      docker push jack.hc-sc.gc.ca/devops/raytest/app_image:latest
       docker tag proxy_image:latest jack.hc-sc.gc.ca/devops/raytest/proxy_image:latest
       docker push jack.hc-sc.gc.ca/devops/raytest/proxy_image:latest
     EOT
     }
     depends_on = [
-      docker_image.server_image, docker_image.client_image, docker_image.proxy_image
+      docker_image.app_image, docker_image.proxy_image
     ]
 }
 
-resource "docker_container" "server_container" { 
-  name  = "server_container"
-  image = docker_image.server_image.name
+resource "docker_container" "app" { 
+  name  = "app"
+  image = docker_image.app_image.name
 
 
   networks_advanced {
@@ -77,22 +70,22 @@ resource "docker_container" "server_container" {
   depends_on = [docker_network.rays-network, null_resource.docker_push]
 }
 
-resource "docker_container" "client_container" {
-  name  = "client_container"
-  image = docker_image.client_image.name
+# resource "docker_container" "client_container" {
+#   name  = "client_container"
+#   image = docker_image.client_image.name
 
 
-  networks_advanced {
-    name = "rays-network"
-  }
+#   networks_advanced {
+#     name = "rays-network"
+#   }
 
-  # ports{
-  #   # external = 82
-  #   internal = 80
-  # }
+#   # ports{
+#   #   # external = 82
+#   #   internal = 80
+#   # }
 
-  depends_on = [docker_network.rays-network, null_resource.docker_push]
-}
+#   depends_on = [docker_network.rays-network, null_resource.docker_push]
+# }
 
 
 
