dfe897166aca0a74634897d2fecc1c41372b7f5c
Raymond Abid <raymond.abid@hc-sc.gc.ca>
2022-09-27 12:25:44 -0700

    moving api container to port 8081

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors')
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
-const PORT = 8080;
+const PORT = 8081;
 
 app.use(cors())
 // app.use(express.static('public'));
